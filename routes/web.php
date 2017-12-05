c46b29c7cc58de5bf6c4d7b681ecbb93834417d8
iuun <280656495@qq.com>
2017-12-06 03:22:43 +0800

    Finish user signup

--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,15 @@ Route::get('/about', 'StaticPagesController@about')->name('about');
 
 Route::get('/signup', 'UsersController@create')->name('signup');
 //这里的name可以随意起名，代表的是这个路由的名称，方便在模板中调用
+
+Route::resource('users', 'UsersController');
+//resource会直接生成一整套路由，等同于：
+/*
+Route::get('/users', 'UsersController@index')->name('users.index');
+Route::get('/users/{user}', 'UsersController@show')->name('users.show');
+Route::get('/users/create', 'UsersController@create')->name('users.create');
+Route::post('/users', 'UsersController@store')->name('users.store');
+Route::get('/users/{user}/edit', 'UsersController@edit')->name('users.edit');
+Route::patch('/users/{user}', 'UsersController@update')->name('users.update');
+Route::delete('/users/{user}', 'UsersController@destroy')->name('users.destroy');
+*/
