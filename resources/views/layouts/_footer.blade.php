ce97ea4c8436284ec4747fc363dba6240d566194
iuun <280656495@qq.com>
2017-12-14 12:37:56 +0800

    footer

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -2,13 +2,13 @@
   <footer class="footer">
     <small class="slogon">
       <img class="brand-icon" src="https://dn-phphub.qbox.me/uploads/images/201612/12/1/iq7WQc2iuW.png?imageView2/1/w/34/h/34">
-      <a href="https://yousails.com/">
+      <a href="#">
         Develope By : Liujun | 微信：router2099
       </a>
     </small>
     <nav>
       <ul>
-        <li><a href="{{ route('about') }}">关 于</a></li>
+        <li><a href="{{ route('about') }}"></a></li>
       </ul>
     </nav>
   </footer>
