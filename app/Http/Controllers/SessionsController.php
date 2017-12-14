5406b2577e1d84de74e7b012f60a685805efa1b3
iuun <280656495@qq.com>
2017-12-14 12:15:32 +0800

    Finish user edit, update, index, and destroy actions

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -7,6 +7,13 @@ use Illuminate\Http\Request;
 use Auth;
 class SessionsController extends Controller
 {
+	public function __construct()
+	{
+		$this->middleware('guest', [
+			'only' => ['create']
+		]);
+	}
+
     public function create()
     {
     	return view('sessions.create');
@@ -22,7 +29,7 @@ class SessionsController extends Controller
     	if(Auth::attempt($credentials, $request->has('remember'))){
     		//登陆成功后操作
     		session()->flash('success', '欢迎回来');
-    		return redirect()->route('users.show', [Auth::user()]);
+    		return redirect()->intended(route('users.show', [Auth::user()]));
     	} else {
     		//登陆失败后操作
     		session()->flash('danger', '抱歉，邮箱密码不匹配，登陆失败');
--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -1,6 +1,7 @@
 <?php
 
 use Faker\Generator as Faker;
+//use App\Models\User;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,12 +15,16 @@ use Faker\Generator as Faker;
 */
 
 $factory->define(App\Models\User::class, function (Faker $faker) {
+	$date_time = $faker->date . ' ' . $faker->time;
     static $password;
 
-    return [
+	return [
         'name' => $faker->name,
-        'email' => $faker->unique()->safeEmail,
+        'email' => $faker->safeEmail,
+        'is_admin' => false,
         'password' => $password ?: $password = bcrypt('secret'),
         'remember_token' => str_random(10),
+        'created_at' => $date_time,
+        'updated_at' => $date_time,
     ];
 });
